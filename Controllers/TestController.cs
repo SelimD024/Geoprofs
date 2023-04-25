f2e9799fae569130a19c737300e0db5d9c7cfdff
SelimD024 <73958505+SelimD024@users.noreply.github.com>
2023-04-25 16:49:30 +0200

    ASP.NET gerealiseerd

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Geoprofs.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Testpage()
+        {
+            Response.Headers.Add("My-Custom-header", "Header value");
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Geoprofs.MvcApplication" Language="C#" %>
