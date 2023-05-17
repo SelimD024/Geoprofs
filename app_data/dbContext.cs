26e96ada018d5a7294aed821da8561577e04592e
SelimD024 <73958505+selimd024@users.noreply.github.com>
2023-05-17 17:57:17 +0200

    dbContext toegevoegd voor interactie tussen Models en data

---
+++ app_data/dbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Web.UI;
+using Geoprofs.Models;
+
+namespace Geoprofs.App_Data
+{
+    public class GeoContext : DbContext
+    {
+        public GeoContext() : base("DefaultConnection")
+        {
+        }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Verlof> Verloven { get; set; } 
+    }
+}
