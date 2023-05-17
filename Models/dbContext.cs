e1f6bcc0cdbc848fe6226c25ddc303aa3c67b6f0
Selim <73958505+SelimD024@users.noreply.github.com>
2023-05-17 20:01:06 +0200

    DbContext locatie veranerd

---
+++ Models/dbContext.cs
@@ -0,0 +1,17 @@
+using System.Data.Entity;
+using System.Web.UI;
+using Geoprofs.Models;
+
+namespace Geoprofs.Models 
+{ 
+    public class dbContext : DbContext
+    {
+        public dbContext() : base("GeoDatabase")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Verlof> Verloven { get; set; }
+    }
+}
