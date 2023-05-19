24963f101e874278d3a9d894424c5bdf0b45e193
SelimD024 <73958505+selimd024@users.noreply.github.com>
2023-05-19 20:24:03 +0200

    Route config file aangemaakt.

---
+++ GeoCore/Routes/config.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+using Microsoft.EntityFrameworkCore;
+
+public static class RouteConfig
+{ 
+    public static void SetRoutes(WebApplication app)
+    {
+        // Home controller
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        // {controller=Forms}/
+        
+    }
+}   
+
+        
