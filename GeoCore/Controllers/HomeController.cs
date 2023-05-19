c0c3b43f0af7f5c3da9f106cd03e1191ddd46bec
Selim <73958505+SelimD024@users.noreply.github.com>
2023-05-19 20:24:29 +0200

    Post controller toegevoegd.

--- GeoCore/Controllers/HomeController.cs
+++ GeoCore/Controllers/HomeController.cs
@@ -1,27 +1,57 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GeoCore.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GeoCore.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly GeoContext _context;
+    
+    
+    public HomeController(ILogger<HomeController> logger, GeoContext context)
     {
+        _context = context;
         _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(Verlof verlof)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(verlof);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(verlof);
     }
 
     public IActionResult Index()
     {
-        return View();
+        return View(new Verlof());
     }
 
     public IActionResult Privacy()
     {
+        
         return View();
     }
+
+    public IActionResult Viewdata()
+    {
+        var items = _context.Verloven.ToList();
+
+        if (items == null)
+        {
+            items = new List<Verlof>();
+        }
+
+        return View(items);
+    }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
