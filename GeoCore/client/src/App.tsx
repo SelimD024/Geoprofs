542c291c8fbc92f708789d727e56c23d899d21d7
Stijn ter haar <stijn.ter.haar2004@gmail.com>
2023-06-07 10:10:02 +0200

    React client toegevoegd

---
+++ GeoCore/client/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./Components/navbar";
+import "./App.css";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="body">
+
+       <Navbar active="dashboard" />
+
+      </div>
+    </>
+  );
+}
+
+export default App;
