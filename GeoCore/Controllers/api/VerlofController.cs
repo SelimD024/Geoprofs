228ef0c8056111c13c7fbba400e4330142908921
Selim <73958505+SelimD024@users.noreply.github.com>
2023-06-21 20:10:08 +0200

    Verlofcontroller aangemaakt, bevat POST en GET

---
+++ GeoCore/Controllers/api/VerlofController.cs
@@ -0,0 +1,50 @@
+﻿using GeoCore.Migrations;
+using GeoCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoCore.Controllers.api;
+
+[Route("api/[controller]")]
+[ApiController]
+
+
+public class VerlofController : ControllerBase
+{
+    private readonly ILogger<VerlofController> _logger;
+    private readonly GeoContext _context;
+    
+    public VerlofController(ILogger<VerlofController> logger, GeoContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+
+    /// <summary>
+    /// Post method 
+    /// </summary>
+    /// <param name="verlof"></param>
+    /// <returns></returns>
+    
+    [HttpPost]
+    public async Task<IActionResult> Verlof([FromBody] Verlof verlof)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        _context.Verloven.Add(verlof);
+        await _context.SaveChangesAsync();
+
+        return Ok(verlof);
+    }
+
+    [HttpGet]
+    public async Task <ActionResult<IEnumerable<Verlof>>> GetAllVerlof()
+    {
+        return await _context.Verloven.ToListAsync();
+    }
+
+}
