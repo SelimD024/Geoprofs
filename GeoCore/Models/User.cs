77778eb2ff31a5fe38304b05bca835347a0551f6
Selim <73958505+SelimD024@users.noreply.github.com>
2023-09-14 19:48:22 +0200

    User model modified

--- GeoCore/Models/User.cs
+++ GeoCore/Models/User.cs
@@ -1,11 +1,16 @@
-﻿namespace  GeoCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace  GeoCore.Models
 {
     public class User
     {
-        public int? Id { get; set; }
-        public string? Username { get; set; }
+        [Key]
+        public int? UserId { get; set; }
+        public int? Role { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
         public string? Password { get; set; }
-        public double? Verlofsaldo { get; set; }
-        public int? Rank { get; set; }
     }
 }
+
