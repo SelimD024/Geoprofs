08af140d9307d051552417a773041659121291b1
Selim <73958505+SelimD024@users.noreply.github.com>
2023-09-27 21:07:04 +0200

    Auth folder met dashboard controller toegevoegd (Dummy voor testen)

---
+++ GeoCore/Controllers/authorization/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using GeoCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GeoCore.Controllers.authorization;
+[Route("authorization/[controller]")]
+[ApiController]
+[Authorize(Roles = "1")]
+public class DashboardController : ControllerBase
+{
+    public IActionResult Dashboard()
+    {
+        return new ContentResult()
+        {
+            Content = "Je hebt toegang",
+            StatusCode = 200
+        };
+    }
+}
